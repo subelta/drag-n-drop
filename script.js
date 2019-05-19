f5081b8ccd2cc71ee783f12f6b8b267878c42a6e
subelta <40162440+subelta@users.noreply.github.com>
2019-05-19 12:58:43 +0300

    proper size for drop place, refactoring

--- script.js
+++ script.js
@@ -1,136 +1,101 @@
 "use strict"
 /**
  * [DragScope]
- * @param  {[String]} draggableClass [класс перетаскиваемого элемента]
- * @param  {[String]} dropFieldClass [класс элемента, куда непосредственно можно положить перетаскиваемый элемент]
- * @param  {[String]} extDropFieldClass [класс элемента, при наведении на который возможен перенос в dropFieldClass
+ * @param  {[String]} draggableClass [HTML класс перетаскиваемого элемента]
+ * @param  {[String]} dropFieldClass [HTML класс элемента, куда непосредственно можно положить перетаскиваемый элемент]
+ * @param  {[String]} extDropFieldClass [HTML класс элемента, при наведении на который возможен перенос в dropFieldClass
  *                                     (внутри него должен быть ОДИН dropFieldClass)]
- * @param  {[String]} highlightClass [класс со стилями для подсветки свободного для перетаскивания места]
- * @param  {[String]} dragStyleClass [класс со стилями для отображения переносимого елемента]
+ * @param  {[String]} highlightClass [HTML класс со стилями для подсветки свободного для перетаскивания места]
+ * @param  {[String]} dragStyleClass [HTML класс со стилями для отображения переносимого елемента]
  * @return {[undefined]}
  */
 var DragScope = function(draggableClass, dropFieldClass, extDropFieldClass, highlightClass, dragStyleClass) {
-
   var elemObject = {};
-  var self = this;
-  var highlighted = false;
+  var dropPlace = false;
 
 
   function onMouseDown(e) {
-    if (e.button != 0) { 
+    var element = e.target.closest("." + draggableClass);
+
+    if ((e.button != 0) || (!element)){ 
         return; 
     } 
 
-    var element = e.target.closest("." + draggableClass);
-    if (!element) {
-      return;
-    }
-
-    element.ondragstart = function() {
-      return false;
-    };
-
     elemObject.oldZindex = element.style.zIndex;
-    elemObject.element = element;
-    elemObject.initialX = e.pageX; //начальные коориднаты зажатия
-    elemObject.initialY = e.pageY;
+    elemObject.el = element;
+    //начальные коориднаты зажатия
+    elemObject.initX = e.pageX; 
+    elemObject.initY = e.pageY;
     return false;
   }
 
-  function onMouseMove(e) {
-    if (!elemObject.element) {
-      return;
-    }  // элемент не зажат
 
-    if(!elemObject.dragging) {
-      var moveX = e.pageX - elemObject.initialX;
-      var moveY = e.pageY - elemObject.initialY;
+
+  function onMouseMove(e) {
+    // элемент не зажат
+    if (!elemObject.el) {
+      return;
+    }  
+
+    if(!elemObject.isDragged) {
+      var moveX = e.pageX - elemObject.initX;
+      var moveY = e.pageY - elemObject.initY;
       if (Math.abs(moveX) < 3 && Math.abs(moveY) < 3) {
         return;
       }
-      elemObject.dragging = true;
-
-      if (!elemObject.element) { // отмена переноса, нельзя "захватить" за эту часть элемента
-        elemObject = {};
-        return;
-      }
+      elemObject.isDragged = true;
       
       // shiftX/shiftY - разница между координатами левого верхнего угла и точкой зажатия
-      var coords = getCoords(elemObject.element);
-      elemObject.shiftX = elemObject.initialX - coords.left;
-      elemObject.shiftY = elemObject.initialY - coords.top;
-      startDrag(e); // начало переноса
+      var coords = getCoords(elemObject.el);
+      elemObject.shiftX = elemObject.initX - coords.left;
+      elemObject.shiftY = elemObject.initY - coords.top;
+      //сохраняем изначальные (вычисленные) размеры элемента в тч padding и border-width для dropPlace
+      elemObject = saveSizes(elemObject); 
+
+      //подсветка места дропа
+      dropPlace = setDropPlace(elemObject,draggableClass, highlightClass);
+      elemObject.el.parentElement.insertBefore(dropPlace, elemObject.el);
+      elemObject.el = startDrag(elemObject, dragStyleClass);
     }
-    elemObject.element.style.pointerEvents = 'none';
-    elemObject.element.style.left = e.pageX - elemObject.shiftX + 'px'; //перенос объекта при каждом движении мыши
-    elemObject.element.style.top = e.pageY - elemObject.shiftY + 'px';
+
+    elemObject.el.style.pointerEvents = 'none';
+    //перенос объекта при каждом движении мыши
+    elemObject.el.style.left = e.pageX - elemObject.shiftX + 'px'; 
+    elemObject.el.style.top = e.pageY - elemObject.shiftY + 'px';
     return false;
   }
 
 
-  function onMouseUp(e) {
-    if (elemObject.dragging) { // если перенос идет
-      self.onDragEnd(e, elemObject);
+
+  function onMouseUp() {
+    if (elemObject.isDragged) {
+      dropElement(elemObject, dropPlace, dragStyleClass);
+      dropPlace = false;
     }
     elemObject = {};
+    return false;
   }
 
 
+
   function onMouseOver(e) {
-    var el = elemObject.element;
+    var el = elemObject.el;
     var target = e.target;  
   
     if (!el) {
       return;
     }
-    if (target == highlighted) {
+    if (target == dropPlace) {
       return;
     } 
 
-
     if (target.classList.contains(extDropFieldClass)) {
       var dropZone = target.querySelector("." + dropFieldClass);
-      dropZone.appendChild(highlighted);
+      dropZone.appendChild(dropPlace);
     } else if (target.classList.contains(draggableClass)) {
-      target.parentElement.insertBefore(highlighted, target);
+      target.parentElement.insertBefore(dropPlace, target);
     }
-  }
-
-
-  function startDrag(e) {
-    var el = elemObject.element;
-    setHighlighted();
-    
-    el.parentElement.insertBefore(highlighted, el);
-    document.body.appendChild(el);
-    elemObject.element.style.zIndex = 9999;
-    el.style.position = 'fixed';
-    el.classList.add(dragStyleClass);
-  }
-
-  
-  this.onDragEnd = function(event, elemObject) {
-    var el = elemObject.element;
-    el.style.pointerEvents = 'auto';
-    el.style.position = "static";
-    el.classList.remove(dragStyleClass);
-    elemObject.element.style.zIndex = elemObject.oldZindex;
-
-    highlighted.replaceWith(el);
-    highlighted = false;
-  };
-
-
-  function setHighlighted() {
-    var el = elemObject.element;
-
-    if (!highlighted) {
-      highlighted = document.createElement(el.tagName);
-      highlighted.classList.add(highlightClass);
-      highlighted.classList.add(draggableClass);
-      highlighted.style.height = el.offsetHeight;
-      highlighted.style.width = el.offsetWidth;
-    }
+    return false;
   }
 
   //не addEventListener чтобы не слушались посторонние события, в данном случае выделение текста
@@ -138,21 +103,80 @@ var DragScope = function(draggableClass, dropFieldClass, extDropFieldClass, high
   document.onmouseup = onMouseUp; 
   document.onmousedown = onMouseDown;
   document.onmouseover = onMouseOver;
-
-  //Задается при подключении на страницу
 };
 
 
-function getCoords(element) { // кроме IE8-
-  var box = element.getBoundingClientRect();
 
+
+function saveSizes(elemObject) {
+  var el = elemObject.el;
+  var elStyle = window.getComputedStyle(el);
+
+  elemObject.width = elStyle.getPropertyValue('width');
+  elemObject.height = elStyle.getPropertyValue('height');
+  elemObject.padding = elStyle.getPropertyValue('padding');
+  elemObject.borderWidth = elStyle.getPropertyValue('border-width');
+  
+  return elemObject;
+}
+
+
+
+function startDrag(elemObject, dragStyleClass) {
+  var el = elemObject.el;
+  
+  el.style.height = elemObject.height;
+  el.style.width = elemObject.width;
+  el.style.zIndex = 9999;
+  el.style.position = 'fixed';
+  el.classList.add(dragStyleClass);
+
+  document.body.appendChild(el);
+  return el;
+}
+
+
+
+function dropElement(elemObject, dropPlace, dragStyleClass) {
+  var el = elemObject.el;
+
+  el.style.pointerEvents = 'auto';
+  el.style.position = "static";
+  el.style.height = '';
+  el.style.width = '';
+  el.classList.remove(dragStyleClass);
+  elemObject.el.style.zIndex = elemObject.oldZindex;
+
+  dropPlace.replaceWith(el);
+}
+
+
+
+function setDropPlace(elemObject, draggableClass, highlightClass) {
+  var el = elemObject.el;
+  var dropPlace; 
+  
+  if (!dropPlace) {
+    dropPlace = document.createElement(el.tagName);
+    dropPlace.classList.add(highlightClass, draggableClass);
+    dropPlace.style.height = elemObject.height;
+    dropPlace.style.width = elemObject.width;
+    dropPlace.style.padding = elemObject.padding;
+    dropPlace.style.borderWidth = elemObject.borderWidth;
+  }
+  return dropPlace;
+}
+
+
+
+function getCoords(element) {
+  var box = element.getBoundingClientRect();
   return {
-    // top: box.top + pageYOffset,
-    // left: box.left + pageXOffset
     top: box.top ,
     left: box.left
   };
 }
+
 
 
 function insertAfter(elem, refElem) {
